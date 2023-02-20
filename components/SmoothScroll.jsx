c4c6d51cc351d9019f2bc943e51e76321e3eea61
0xtter <thomas.desrumeaux@gmail.com>
2023-02-20 14:59:48 +0100

    Added smooth scroll for the lottie file

--- components/SmoothScroll.jsx
+++ components/SmoothScroll.jsx
@@ -1,7 +1,10 @@
 export function scrollToSection(event) {
     event.preventDefault();
     try {
-        const section = document.querySelector(event.target.parentElement.getAttribute('href'));
+        var section = document.querySelector(event.target.parentElement.getAttribute('href'));
+        if (section == null) { // For Lottie Animations
+            section = document.querySelector(event.target.getAttribute('href'));
+        }
         section.scrollIntoView({ behavior: "smooth" });
     } catch (error) {
         console.error(error);
